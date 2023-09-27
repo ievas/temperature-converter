bc7ac4e5df21c7aa374ca5a342193614ecfbf5e3
ievas <ieva.ritina@gmail.com>
2023-09-27 13:15:03 -0600

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,13 @@
  * @param {string} fahren temperature in degrees F
  * @returns {number} the number of degrees C
  */
-function convertToCelsius(fahren) {}
+function convertToCelsius(fahren) {
+  
+  let F = Number(fahren);
+  let C = (F - 32) * 5/9;
+
+  return C;
+}
 
 /**
  * Takes both numbers (F, C) and display a message with
@@ -22,14 +28,34 @@ function convertToCelsius(fahren) {}
  * @param {number} fahren
  * @param {number} celc
  */
-function createMessage(fahren, celc) {}
+
+// num.toFixed(2) rounds Celsius to two decimal places
+
+function createMessage(fahren, celc) {
+
+  if (fahren <= 32 ) {
+    return alert(`${fahren}°F or ${+celc.toFixed(2)}°C feels very cold`);
+  } else if (fahren <= 64) {
+    return alert(`${fahren}°F or ${+celc.toFixed(2)}°C feels cold`);
+  } else if (fahren <= 86) {
+    return alert (`${fahren}°F or ${+celc.toFixed(2)}°C feels warm`);
+  } else if (fahren < 100) {
+    return alert (`${fahren}°F or ${+celc.toFixed(2)}°C feels hot`);
+  } else  {
+    alert (`${fahren}°F or ${+celc.toFixed(2)}°C too hot`)
+  }
+
+}
 
 /**
  * Takes a number and returns a random integer from 0 to the limit
  * @param {number} limit
  * @returns {number} a number between 0 and the int passed in
  */
-function rand(limit) {}
+function rand(limit) {
+  let number = Math.floor(Math.random(0) * limit);
+  return number;
+}
 
 // -------------------- DONT NOT CHANGE THE CODE BELOW ---------------------- //
 
